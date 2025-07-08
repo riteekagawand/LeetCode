fff06d2b8a614552fd0ae0aff7505817763fe350
Riteeka Gawand <128922942+riteekagawand@users.noreply.github.com>
2025-07-08 19:45:17 +0530

    Time: 5 ms (48.31%), Space: 18 MB (24.8%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        grid = defaultdict(set)
+        rows = {i:set() for i in range(9)}
+        cols = {i:set() for i in range(9)}
+        grid = {(i,j):set() for i in range(3) for j in range(3)}
 
         for r in range(9):
             for c in range(9):
