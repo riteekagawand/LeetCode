c17d3dd323be0ebf6442447dd0621741b17433cc
Riteeka Gawand <128922942+riteekagawand@users.noreply.github.com>
2025-07-09 20:33:33 +0530

    Time: 266 ms (69.16%), Space: 18 MB (44.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,19 +1,19 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        max_len = 0
-        longest = ""
-
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expandCenter(left,right):
+            while left >= 0 and right < len(s) and s[left]==s[right]:
+                left-=1
+                right+=1
+            return s[left+1:right]
+        longest=""
         for i in range(len(s)):
-            for j in range(i, len(s)):
-                substring = s[i:j+1]
-                if substring == substring[::-1]:
-                    if max_len < len(substring):
-                        max_len = len(substring)
-                        longest = substring
-        return longest
-
-        """
-        :type s: str
-        :rtype: str
-        """
+            temp1 = expandCenter(i,i)
+            temp2 = expandCenter(i,i+1)
         
+            if len(temp1) > len(longest):
+                longest = temp1
+            
+            if len(temp2) > len(longest):
+                longest = temp2
+        return longest
+            
